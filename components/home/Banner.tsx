d5fd4731df385b9f81b3ab1579abc1ddd52a2ed2
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2023-01-03 00:50:13 -0300

    🚧
    test de produção

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -8,7 +8,7 @@ export const Banner = () => {
         muted
         autoPlay
         loop
-        src='https://vod-progressive.akamaized.net/exp=1672623536~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F55%2F16%2F400276677%2F1708047268.mp4~hmac=c9116c5c4db7d0ae63e55d1280e6efdc56685f88c97dde24b46c5d57bf160e9a/vimeo-prod-skyfire-std-us/01/55/16/400276677/1708047268.mp4'
+        src='https://1894e86a-153e-444e-932f-0a3327f905c9.id.repl.co/video.mp4'
         className='w-full h-screen object-cover brightness-50 saturate-50 '
       />
       <div className='absolute flex flex-col justify-center items-center gap-3 text-center font-bold  text-5xl'>
